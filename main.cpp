9ae3ae37f56d6d414f8ca40df19dbc1f7e4bc5cf
Goodkid01 <zaikinbkmkop@gmail.com>
2024-11-30 11:43:34 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -12,22 +12,13 @@ int main()
     int p=1;
     int sum=0;
     int q=0;
-    int q1=1;
-    q1=n%10;
-    if(q1%2!=0)
+    while(n>0)
     {
-        sum=sum+q1;
-    }
-    while(n/=10)
-        {
         int q=n%10;
         if(q%2!=0)
-        {
-            sum=sum+q;
-        }
-        }
-    std::cout<<n<<std::endl;
-    
-
+            sum += q;
+        n /= 10;
+    }
    std::cout<<sum<<std::endl;
+   
 }
