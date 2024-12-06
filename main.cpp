9d79944f724b024b889d1aaf11537a5cadc11272
Goodkid01 <zaikinbkmkop@gmail.com>
2024-12-06 21:34:45 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -7,18 +7,54 @@
 
 int main() 
 {
-    int n;
-    std:: cin>> n;
-    int p=1;
-    int sum=0;
-    int q=0;
-    while(n>0)
-    {
-        int q=n%10;
-        if(q%2!=0)
-            sum += q;
-        n /= 10;
-    }
-   std::cout<<sum<<std::endl;
+   // int n;
+    //std:: cin>> n;
+    //int p=1;
+    //int sum=0;
+    //int q=0;
+    //while(n>0)
+   // {
+     //   int q=n%10;
+       // if(q%2!=0)
+         //   sum += q;
+        //n /= 10;
+    
+   //std::cout<<sum<<std::endl;
    
+// const int x=1000;
+// int mas[x];
+int maxx=-10000;
+int sum=0;
+int n;
+int maxxi=-1;
+std::cin>>n;
+int u=0;
+for (int i=0;i<n;i++)
+    {
+    std::cin>>u;
+    if((u%10==0) || (u%10==7))
+    {
+        sum=sum+u;
+        if(u>maxx)
+        {
+            maxx=u;
+            //maxxi=i;
+        }
+
+    }
+    }
+std::cout<<maxx<<"   "<<sum<<std::endl;
+
+
+
+
+
+
+
+
+
+
+
+
 }
+
